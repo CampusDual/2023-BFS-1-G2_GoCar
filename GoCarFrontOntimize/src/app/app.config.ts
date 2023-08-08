a29c0bb42cbe3128f8ce74785295d3e504e4649e
Pablo Martínez Kirsten <pablo.martinez@imatia.com>
2023-08-08 08:47:59 +0200

    Add Dockerfiles for DB, backend and frontend. Adapt SLQ dump to restore DB. Adapt backend and frontend apps to run from docker

--- GoCarFrontOntimize/src/app/app.config.ts
+++ GoCarFrontOntimize/src/app/app.config.ts
@@ -6,8 +6,8 @@ import { SERVICE_CONFIG } from './shared/app.services.config';
 export const CONFIG: Config = {
   // The base path of the URL used by app services.
   //apiEndpoint: 'https://try.imatia.com/ontimizeweb/services/qsallcomponents-jee/services/rest',
-  apiEndpoint: 'http://localhost:33333',
-  
+  apiEndpoint: 'http://localhost:30069',
+
   // bundle: {
   //   path: 'bundle'
   // },
@@ -31,7 +31,7 @@ export const CONFIG: Config = {
 
   appMenuConfiguration: MENU_CONFIG,
 
-  applicationLocales: ['es', 'en','gl'],
+  applicationLocales: ['es', 'en', 'gl'],
 
   exportConfiguration: {
     path: '/export'
